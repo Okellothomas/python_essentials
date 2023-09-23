7e9fdb836f16835f0916c327eb95170dc756e74b
Okellothomas <thomasolal33@gmail.com>
2023-09-23 21:46:28 +0300

    add inbuilt methods

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ print(name)
 print(name + ' Okey')
 age = 180
 print(name + "okello is", age)
+
+# Python Inbuilt functions
+scores = "thomas okello olal"
+print(scores[4])
+print(scores.capitalize())
+print(scores.islower())
+print(scores.upper())
+print(scores.isupper())
+print(scores.lower())
+print(scores.__len__())
