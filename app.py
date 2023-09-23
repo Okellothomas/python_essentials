da810ff6a6a7418cd889a180f2cfc0dc47ad0ddc
Okellothomas <thomasolal33@gmail.com>
2023-09-23 21:37:59 +0300

    getting started with python programming

---
+++ app.py
@@ -0,0 +1,10 @@
+# Printing the first hello world in my data
+print("Hello World")
+# Second hello world data
+print("Hello thomas, i am welcomed!")
+# Variables
+name = 'Okello Thomas'
+print(name)
+print(name + ' Okey')
+age = 180
+print(name + "okello is", age)
