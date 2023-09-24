58b50a4475e1fbddedd8c914691bd5288b021bc7
Okellothomas <thomasolal33@gmail.com>
2023-09-24 06:06:32 +0300

    the tuple class is a good one

---
+++ tupple.py
@@ -0,0 +1,9 @@
+# Tupples are just immutable lists
+
+tuple = tuple(("Okello", False, 90, "Thomas"))
+tuple1 = (3, 2, "Samwel", True, "903")
+
+# Operations on tuples
+print(len(tuple))
+print(tuple[0])
+print(type(tuple1))
