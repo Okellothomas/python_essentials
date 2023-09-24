99ad581bc1b57c73067a9e074ba9385005d47e02
Okellothomas <thomasolal33@gmail.com>
2023-09-24 04:57:30 +0300

    add lists to the program

--- input.py
+++ input.py
@@ -1,20 +1,20 @@
 from math import *
 
-# name = str(input("Enter your name please: "))
-# age = int(input("Enter your age please: "))
-# testAnswer = sqrt(int(input("Enter the squaroot of 100: ")))
+name = str(input("Enter your name please: "))
+age = int(input("Enter your age please: "))
+testAnswer = sqrt(int(input("Enter the squaroot of 100: ")))
 
-# print("Your name is: " + name + " And your are", age,
-#       " old", " and the right answer is ", testAnswer)
+print("Your name is: " + name + " And your are", age,
+      " old", " and the right answer is ", testAnswer)
 
 
 # simple relacement program
 
-# namei = str(input("Enter your name: "))
-# myage = int(input("Enter your age: "))
-# changeAnge = int(input("Enter your correct age: "))
-# correctAge = namei.replace(myage, changeAnge)
-# print("My Name is " + namei + " and my correct age is ", correctAge)
+namei = str(input("Enter your name: "))
+myage = int(input("Enter your age: "))
+changeAnge = int(input("Enter your correct age: "))
+correctAge = namei.replace(myage, changeAnge)
+print("My Name is " + namei + " and my correct age is ", correctAge)
 
 # the correct replacement program code.
 sentence = input("Enter a sample of a poem: ")
---
+++ list.py
@@ -0,0 +1,3 @@
+# Defining a list
+countries = ['Kenya', "UK", "UG", "US", "Niger", 8, True, 892.992]
+print(countries)
