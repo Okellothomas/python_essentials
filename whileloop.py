ddcc7ce175301aba789e46d330acb0f6eaab6b66
Okellothomas <thomasolal33@gmail.com>
2023-09-24 11:11:25 +0300

    the while loop

---
+++ whileloop.py
@@ -0,0 +1,21 @@
+# While loop is used to iterated over a given value
+
+# i = 20
+
+
+def While_Loop():
+    i = 1
+    while i <= 27:
+        i += 1
+        return i
+
+
+print(While_Loop())
+
+# Second example
+print("*************************************")
+age = 1
+
+while age <= 3:
+    print(age)
+    age += 1
