7e3c257e0ca54ea53ca33a0c8947d63958494ce5
Okellothomas <thomasolal33@gmail.com>
2023-09-24 05:52:41 +0300

    add to the list of python

--- list.py
+++ list.py
@@ -37,3 +37,27 @@ print(list2)
 # clearing the content of the list using the clear keyword
 list2.clear()
 print(list2)
+# find the index of a given element of a list using index method
+index = list1.index(10)
+print(index)
+# find the count of a given element of list using the count method
+count = list1.count(10)
+print(count)
+# present the list element of the list in ascending order
+listtwo.sort()
+print(listtwo)
+# reversing the order other the list using the revers method
+listtwo.reverse()
+print(listtwo)
+# duplicate a list using the copy method
+list3 = list1.copy()
+print(list3)
+# removing the last element of a list using the pop method
+list3.pop()
+print(list3)
+# deleting the first element of a list using the del method
+del list3[0]
+print(list3)
+# deleting an entire list using the del method
+del list3
+print(list3)
