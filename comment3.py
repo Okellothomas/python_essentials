9eeb14891de88623c339550b48381979c07bcdf6
Okellothomas <thomasolal33@gmail.com>
2023-09-24 13:27:28 +0300

    add the comment syntax

---
+++ comment3.py
@@ -0,0 +1,14 @@
+# comments are used for two purposes:
+# 1. code documentation: tell other programmers what each piece of your code does
+
+# 2. Prevent some part of the code from being compiled.
+
+# there are two types of comments:
+
+# Single and double comments:
+# Single comment syntax
+#
+'''Double comment syntax'''
+'''
+I am a double comment
+'''
