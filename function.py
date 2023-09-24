b5bbf9a305dc40d1d9695e16af70e460621590ff
Okellothomas <thomasolal33@gmail.com>
2023-09-24 09:23:06 +0300

    add changes

---
+++ function.py
@@ -0,0 +1,29 @@
+# Functions refers to a set of
+# the Add_Num returns the greatest number among three numbers
+def Add_num(a, b, c):
+    if (a > b and a > c):
+        print(a, "is the largest")
+    elif (b > a and b > c):
+        print(b, "is the largest")
+    else:
+        print(c, "is the largest")
+
+
+Add_num(a=10, b=100, c=103)
+
+# a function that accepts inputs:
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+
+def Greatest(num1, num2, num3):
+    if (num1 > num2 and num1 > num3):
+        print(num1, "is the greatest")
+    elif (num2 > num1 and num2 > num1):
+        print(num2, "is the greatest")
+    else:
+        print(num3, "is the greatest")
+
+
+Greatest(num1, num2, num3)
