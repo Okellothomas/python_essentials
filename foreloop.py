15e8fcdaf9be7b1dd4224d1cc4533d4823d8d894
Okellothomas <thomasolal33@gmail.com>
2023-09-24 11:28:51 +0300

    using else in the forloop

---
+++ foreloop.py
@@ -0,0 +1,18 @@
+# For loop is also used for iteration just like the while loop, however, it is more powerful when compared to the while loop
+
+number = "Hello numbers"
+
+for num in number:
+    print(num)
+
+for num in range(1, 11):
+    print("I love you mum")
+
+# We can use break to terminate the iteration in python
+
+for num in range(8):
+    if (num == 7):
+        break
+    print(num)
+
+# using else in for loop
