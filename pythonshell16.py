b75a0c81bde587d5218c58b6e1e04556c1e3bd77
Okellothomas <thomasolal33@gmail.com>
2023-10-02 15:15:17 +0300

    add changes

---
+++ pythonshell16.py
@@ -0,0 +1,6 @@
+# Python shell or python interpretor allow us to write code and execute them just like we would do in the our
+'''
+Visual studio, 
+* It is great way to write code. 
+to get to the shell we simply type python
+'''
