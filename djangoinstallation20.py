98455d78de54e2d215acac858820764dfaafb988
Okellothomas <thomasolal33@gmail.com>
2023-10-03 08:32:38 +0300

    add changes

---
+++ djangoinstallation20.py
@@ -0,0 +1,23 @@
+# Getting started with a django project
+'''
+1. we start by installing python along with other important packages such as pip. 
+2. pip install django to install the django package 
+3. pip install virtualenvwrapper-win to instal the virtual environment 
+4. mkvirtualevn venv to create a virtual environment for the project
+5. pip install django to install django in the virtual environment 
+6. django-admin startproject myproject to create a new django project called myproject
+'''
+
+# The key components of a django applications.
+'''
+1. The manage.py helps with running the project, migrating databases and should not be intefere with especially
+for beginners 
+2. setting.py, this is where all the setting for the project will take place. 
+3. url.py, this contains all the url for the projects. 
+'''
+
+# activating and deactivating the virtual enviroment
+'''
+deactivate - to deactivate the virtual environment
+workon venv - to activate the virtual environment. 
+'''
